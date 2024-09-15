897cb7cd79cb1100d8932625b58c1b8a809f5401
Omid Tahmasebi <tomid7373@gmail.com>
2024-09-15 23:32:46 +0330

    commit (1)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook , faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  Axios.defaults.headers['X-Api-Key'] = 'E+5FjbdBewMMRcYv47G6uA==ZSiuPVzynA8AVZoo';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header /> 
+      <Main />
     </div>
   );
+}
+
+const Header = ()=>{
+  return(
+    <header className='header'>
+      <h1 className='title'><FontAwesomeIcon icon={faBook} /> Read Quotes</h1>
+      <p className='developer'>omid Tahmasebi Boldaji</p>
+    </header>
+  )
+}
+
+const Main = ()=>{
+  const [quote , setQuote] = useState('')
+  const [author , setAuthor] = useState('')
+
+  useEffect(()=>{
+    Axios.get('https://api.api-ninjas.com/v1/quotes').then((res)=>{
+      setQuote(res.data[0].quote)
+      setAuthor(res.data[0].author)
+    })
+  } , [])
+
+  const nextQuote = ()=>{
+    Axios.get('https://api.api-ninjas.com/v1/quotes').then((res)=>{
+      setQuote(res.data[0].quote)
+      setAuthor(res.data[0].author)
+    })
+  }
+
+  return(
+    <main className='main'>
+      <p className='quote'> {quote} </p>
+      <h4 className='author'> {author} </h4>
+      <button className='nextButton' onClick={nextQuote}><FontAwesomeIcon icon={faArrowRight} /></button>
+    </main>
+  )
 }
 
 export default App;
